42e037acac85b103bbd997fc22744b2d0811e053
emi-dt <satoe092@newschool.edu>
2019-09-05 21:43:23 -0400

    updating js file

--- week2/assets/js/main.js
+++ week2/assets/js/main.js
@@ -1,4 +1,82 @@
 
+let images = [
+    "newschoolbuilding.jpg",
+    "050615_Festival_052_750x563.jpg",
+    "745127-original.jpg",
+    "1496605813404.jpeg",
+    "bigimg_parsons.png",
+    "D10_01.jpg",
+    "DnKbS0IX4AEBA5-.jpg",
+    "exterior-PortalImages.jpg",
+    "newschoolbuilding.jpg",
+    "Parsons_about_5607thave.jpg",
+    "Parsons-about.jpg",
+    "SP16_SummerIntensive_Nector_Parsons.jpg",
+    "The-New-School-Student-Life-3.jpg",
+    "The-New-School’s-Parsons-School-of-Design-3.jpg",
+    "tish.jpg"];
+
+let imgSwitch = 0;
+
+let popup = document.getElementById('popup');
+    
+
+//Loading images ------------
+function loadImages(){
+    let imageNumber = 0;
+
+    let columns = document.getElementsByClassName('column');
+    // console.log(columns.length);
+    for( let i=0;  i < images.length; i++){
+        let newImg = document.createElement('img');
+        newImg.className = 'images';
+        newImg.id = i;
+        newImg.src = './assets/images/'+images[i];
+
+        columns[imageNumber].appendChild(newImg);
+        imageNumber++;
+
+        //every 3 colums, reset to 0
+        if(imageNumber > columns.length - 1){
+            imageNumber = 0;
+        }
+
+        newImg.addEventListener('click', function(tempValues){
+            popUp(tempValues.target.id);
+            imgSwitch = tempValues.target.id;
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('JS is working!');
+    loadImages();
+});
+
+
+//Image POP UP ----------------------
+function popUp(imageCount){
+    imgSwitch = imageCount;
+
+    // var popup = document.getElementById('popup');
+    var img = document.getElementById('pic');
+
+    popup.style.zIndex = '1';
+    popup.style.display = 'block';
+    img.src = './assets/images/' + images[imageCount];
+}
+
+
+function close(){
+    popup.style.zIndex = '1';
+    popup.style.display = 'none'
+}
+
+
+
+
+
+
 // function start(){
 //     for(let i = 0; i < 5; i++ ){
 //         console.log(i);
@@ -47,53 +125,3 @@
  */
 
 // This is the array of the images
-
-let images = [
-    "newschoolbuilding.jpg",
-    "050615_Festival_052_750x563.jpg",
-    "745127-original.jpg",
-    "1496605813404.jpeg",
-    "bigimg_parsons.png",
-    "D10_01.jpg",
-    "DnKbS0IX4AEBA5-.jpg",
-    "exterior-PortalImages.jpg",
-    "newschoolbuilding.jpg",
-    "Parsons_about_5607thave.jpg",
-    "Parsons-about.jpg",
-    "SP16_SummerIntensive_Nector_Parsons.jpg",
-    "The-New-School-Student-Life-3.jpg",
-    "The-New-School’s-Parsons-School-of-Design-3.jpg",
-    "tish.jpg"];
-
-
-    
-
-
-function loadImages(){
-    let imageNumber = 0;
-
-    let columns = document.getElementsByClassName('column');
-    // console.log(columns.length);
-    for( let i=0;  i < images.length; i++){
-        let newImg = document.createElement('img');
-        newImg.className = 'images';
-        newImg.id = i;
-        newImg.src = './assets/images/'+images[i];
-
-        columns[imageNumber].appendChild(newImg);
-        imageNumber++;
-
-        //every 3 colums, reset to 0
-        if(imageNumber > columns.length - 1){
-            imageNumber = 0;
-        }
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('JS is working!');
-    loadImages();
-});
-
-
-
